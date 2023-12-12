cd6e7d41bc8e3dc25e0565d80166195d5afcd218
mastiico <marcinhofilho94@gmail.com>
2023-12-12 07:10:55 -0300

    First commit

---
+++ src/main/java/br/com/mastico/RestFullWithJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.mastico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullWithJavaApplication.class, args);
+	}
+
+}
